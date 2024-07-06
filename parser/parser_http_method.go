e8c1382c1b142102e662622e2c9282a12e564b8e
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-07-06 22:54:29 +0545

    feat: user single http method parser to handle all different http method

---
+++ parser/parser_http_method.go
@@ -0,0 +1,28 @@
+package parser
+
+import "strings"
+
+func parseHttpMethod(tokens []Token, i *int) HTTPNode {
+	req := HTTPNode{}
+	req.Method = tokens[*i].Value
+	req.Headers = make(map[string]string)
+	*i++
+	for tokens[*i].Type != TOKEN_RBRACE {
+		switch tokens[*i].Type {
+		case TOKEN_IDENTIFIER:
+			switch strings.TrimSpace(tokens[*i].Value) {
+			case "url":
+				*i++
+				req.URL = tokens[*i+1].Value
+			case "body":
+				*i++
+				req.Body = tokens[*i+1].Value
+			case "auth":
+				*i++
+				req.Auth = tokens[*i+1].Value
+			}
+		}
+		*i++
+	}
+	return req
+}
