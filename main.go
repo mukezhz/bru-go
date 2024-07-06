c2a82a290c079ff31c150cbd1e175723b1ab3e5c
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-07-06 23:36:34 +0545

    feat: pring after parsing

--- main.go
+++ main.go
@@ -13,10 +13,21 @@ var bruFile string
 func main() {
 	// fmt.Println(bruFile)
 	tokens := parser.Tokenize(bruFile)
-	// for _, token := range tokens {
-	// 	fmt.Println(token)
+	// for i, token := range tokens {
+	// 	fmt.Println(i, token.Type, token.Value)
 	// }
 	ast := parser.Parse(tokens)
-	log.Println(ast)
+	// log.Println(ast)
+	for _, node := range ast {
+		if g, ok := node.(parser.HTTPNode); ok {
+			log.Println(g.Method, g.URL, g.Body, g.Auth, g.Headers)
+		}
+		if g, ok := node.(parser.MetaNode); ok {
+			log.Println(g.Name, g.Type, g.Seq)
+		}
+		if g, ok := node.(parser.BodyNode); ok {
+			log.Println(g.Content)
+		}
+	}
 
 }
