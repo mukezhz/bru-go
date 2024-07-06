8235166eb4e93bc0221ff08617b172f3cbdf6cc8
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-07-06 23:12:14 +0545

    feat: add test for auth tokenizer

--- parser/lexer_test.go
+++ parser/lexer_test.go
@@ -27,6 +27,28 @@ func TestParseBody(t *testing.T) {
 	}
 
 	if tokens[0].Type != body.Type {
-		t.Errorf("Expected bru tag body, got %s", tokens[0].Type)
+		t.Errorf("Expected type __body__, got %s", tokens[0].Type)
+	}
+}
+
+//go:embed auth.bru
+var authBru string
+
+func TestParseAuth(t *testing.T) {
+	auth := parser.Token{
+		Type:  parser.TOKEN_AUTH,
+		Value: "auth",
+	}
+	tokens := parser.Tokenize(authBru)
+	if len(tokens) != 8 {
+		t.Errorf("Expected 8 tokens, got %d", len(tokens))
+	}
+
+	if tokens[0].Value != auth.Value {
+		t.Errorf("Expected bru tag auth, got %s", tokens[0].Type)
+	}
+
+	if tokens[0].Type != auth.Type {
+		t.Errorf("Expected type __auth__, got %s", tokens[0].Type)
 	}
 }
