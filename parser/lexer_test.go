64eff23085c638a3f9f0bee773e97048090e3c05
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-07-06 23:14:23 +0545

    feat: add test for post http method

--- parser/lexer_test.go
+++ parser/lexer_test.go
@@ -74,3 +74,25 @@ func TestParseMeta(t *testing.T) {
 		t.Errorf("Expected type __meta__, got %s", tokens[0].Type)
 	}
 }
+
+//go:embed post.bru
+var postBru string
+
+func TestParsePost(t *testing.T) {
+	post := parser.Token{
+		Type:  parser.TOKEN_POST,
+		Value: "post",
+	}
+	tokens := parser.Tokenize(postBru)
+	if len(tokens) != 12 {
+		t.Errorf("Expected 12 tokens, got %d", len(tokens))
+	}
+
+	if tokens[0].Value != post.Value {
+		t.Errorf("Expected bru tag post, got %s", tokens[0].Type)
+	}
+
+	if tokens[0].Type != post.Type {
+		t.Errorf("Expected type __post__, got %s", tokens[0].Type)
+	}
+}
