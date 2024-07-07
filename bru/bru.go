ce6293743160bc84b6a4cbbf23a73feee0512aea
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-07-07 11:59:55 +0545

    feat: move methods to bru

---
+++ bru/bru.go
@@ -0,0 +1,23 @@
+package bru
+
+import "github.com/mukezhz/bru-go/parser"
+
+func GetTokens(file string) []parser.Token {
+	tokens := parser.Tokenize(file)
+	return tokens
+}
+
+func GetAST(tokens []parser.Token) []parser.ASTNode {
+	ast := parser.Parse(tokens)
+	return ast
+}
+
+func GetTagNode[T any](ast []parser.ASTNode) T {
+	for _, n := range ast {
+		switch n := n.(type) {
+		case T:
+			return n
+		}
+	}
+	return *new(T)
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"log"
 
+	"github.com/mukezhz/bru-go/brugo"
 	"github.com/mukezhz/bru-go/parser"
 	"github.com/mukezhz/bru-go/utils"
 )
@@ -12,11 +13,11 @@ import (
 var bruFile string
 
 func main() {
-	tokens := GetTokens(bruFile)
-	ast := GetAST(tokens)
-	n := GetTagNode[parser.HTTPNode](ast)
+	tokens := brugo.GetTokens(bruFile)
+	ast := brugo.GetAST(tokens)
+	n := brugo.GetTagNode[parser.HTTPNode](ast)
 	log.Println(n.Method, n.URL, n.Body, n.Auth)
-	headers := GetTagNode[[]parser.HeaderNode](ast)
+	headers := brugo.GetTagNode[[]parser.HeaderNode](ast)
 	log.Println(headers)
 	mapHeaders := utils.FromHeaderNodeToMap(headers)
 	log.Println(mapHeaders)
@@ -24,24 +25,4 @@ func main() {
 		log.Println(k, v)
 	}
 
-}
-
-func GetTokens(file string) []parser.Token {
-	tokens := parser.Tokenize(file)
-	return tokens
-}
-
-func GetAST(tokens []parser.Token) []parser.ASTNode {
-	ast := parser.Parse(tokens)
-	return ast
-}
-
-func GetTagNode[T any](ast []parser.ASTNode) T {
-	for _, n := range ast {
-		switch n := n.(type) {
-		case T:
-			return n
-		}
-	}
-	return *new(T)
 }
